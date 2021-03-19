98ec18048194758e00bc4d8b5fbed26e2e531d3c
Emmet Stanevich <emmet@stanevich.com>
2021-03-18 21:08:21 -0400

    Changed email column to be persisted in the database

--- flaskr/models.py
+++ flaskr/models.py
@@ -8,7 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     username = Column(String(length=64), unique=True, nullable=False)
     password = Column(CHAR(length=94), nullable=False)
-    email = Column(Computed("email_local + '@' + email_domain"))
+    email = Column(Computed("email_local + '@' + email_domain", persisted=True))
     email_local = Column(String(length=64), nullable=False)
     email_domain = Column(String(length=255), nullable=False)
     phone = Column(CHAR(10), nullable=False)
