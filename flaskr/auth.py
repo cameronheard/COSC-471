d8c0b4d3efa0ac8184af814350c4ef08f27f36da
Emmet Stanevich <emmet@stanevich.com>
2021-03-18 21:15:41 -0400

    Created login page

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -66,6 +66,9 @@ def login():
             if error is None:
                 session.clear()
                 session["user_id"] = user["id"]
+        return redirect(url_for(None))
+
+    return render_template("auth/login.html", form=LoginForm())
 
 
 @bp.route("/logout")
