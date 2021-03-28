6f6ccfe3283bc2b71ef916b45dd594ece3bcffb3
Emmet Stanevich <emmet@stanevich.com>
2021-03-28 00:25:15 -0400

    Added alternate database file
    This file serves a similar purpose to dbConnection.php, but it instead uses the PDO library. The other file is, of course, still available to use, but I'll be using this file for my other files.

---
+++ src/includes/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @return PDO A new PDO connected to the project database
+ * @throws PDOException If the attempt to connect to the database fails
+ */
+function connect_to_database(): PDO
+{
+    $username = isset($_ENV["MYSQL_USERNAME_FILE"]) ? file_get_contents($_ENV["MYSQL_USERNAME_FILE"]) : $_ENV["MYSQL_USERNAME"] ?? "root";
+    $password = isset($_ENV["MYSQL_PASSWORD_FILE"]) ? file_get_contents($_ENV["MYSQL_PASSWORD_FILE"]) : $_ENV["MYSQL_PASSWORD"];
+    return new PDO("mysql:host={${$_ENV['DB_HOST'] ?? 'localhost'}};dbname={${$_ENV['DB_NAME'] ?? 'eMarket'}};port={${$_ENV['DB_PORT'] ?? 3306}}",
+    $username,
+    $password);
+}
+
+/**
+ * @param $connection PDO The connection to close
+ */
+function close_database_connection(PDO &$connection)
+{
+    $connection = null;
+}
