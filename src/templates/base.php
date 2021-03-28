74a077db7653a9cd69e67bd72c38b721ba1a7f07
Emmet Stanevich <emmet@stanevich.com>
2021-03-27 22:14:55 -0400

    Added navbar to base template
    The navbar should now be present on all pages that use the base template.

--- src/templates/base.php
+++ src/templates/base.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title><?= $title ?></title>
+    <title><?= $title ?></title>
 </head>
 <body>
+<?php include __DIR__ . "/navbar.php" ?>
 <?= $content ?>
 </body>
 </html>
