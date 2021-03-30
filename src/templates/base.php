b9b08ed05ecdb824f101d8a8713f38212df94758
Emmet Stanevich <emmet@stanevich.com>
2021-03-30 18:49:55 -0400

    Added error messages to base template
    If there are any errors the user needs to be made aware of, they will be sent via an alert, and removed from the errors array.

--- src/templates/base.php
+++ src/templates/base.php
@@ -5,6 +5,15 @@
 </head>
 <body>
 <?php include __DIR__ . "/navbar.php" ?>
+<?php if (!empty($_SESSION["errors"])): ?>
+    <script>
+        {
+            let errors = JSON.parse('<?= json_encode($_SESSION["errors"])  // Encodes the list of errors into a JSON array. ?>');  // Reads the provided JSON array from the server.
+            <?php unset($_SESSION["errors"])  // After retrieving the errors, remove them from the session ?>
+            alert(errors.join("\n"));  // Alerts the user to the error(s).
+        }
+    </script>
+<?php endif; ?>
 <?= $content ?>
 </body>
 </html>
