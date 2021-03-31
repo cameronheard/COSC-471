b143ee5584ff41624c32b6afe1aa29be2f173c45
Emmet Stanevich <emmet@stanevich.com>
2021-03-31 13:28:36 -0400

    Added environment variables to dbConnection script
    This should allow for the script to run on my computer, while still letting it run on systems where the MySQL username/password were not declared in environment variables.

--- src/includes/dbConnection.php
+++ src/includes/dbConnection.php
@@ -1,8 +1,29 @@
-<?php 
-$dbHost = "localhost";
-$dbUser = "root";
-$dbPass = "";
+<?php
+/**
+ * The address of the MySQL server. If not set, the connector will default to "localhost"
+ */
+$dbHost = $_ENV["DB_HOST"];
+
+/**
+ * The current MySQL user, defaulting to "root"
+ */
+$dbUser = $_ENV["MYSQL_USERNAME"] ?? "root";
+
+/**
+ * The current MySQL user's password.
+ * If this environment variable isn't set, the connector will authenticate against users without a password
+ * (such as the root user, if you hadn't set a password for it)
+ */
+$dbPass = $_ENV["MYSQL_PASSWORD"];
+
+/**
+ * The name of the database to connect to
+ */
 $db = "eMarket";
 
-$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $db);
- ?>
+/**
+ * The port number of the MySQL server.
+ */
+$dbPort = $_ENV["DB_PORT"];
+
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $db, $dbPort);
