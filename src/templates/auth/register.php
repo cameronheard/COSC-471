c69dd5e617d5cdf2e3548fabf61b661a88b18233
Emmet Stanevich <emmet@stanevich.com>
2021-04-03 16:10:45 -0400

    Changed names for registration fields
    This is mainly to help make it a bit easier to collect all relevant values into a single array (while keeping it separate from the rest of the request, if applicable)

--- src/templates/auth/register.php
+++ src/templates/auth/register.php
@@ -5,50 +5,50 @@
 <table>
     <tr>
         <td><label for="first_name">First Name:</label></td>
-        <td><input id="first_name" name="first_name" required></td>
+        <td><input id="first_name" name="register[first_name]" required></td>
     </tr>
     <tr>
         <td><label for="last_name">Last Name:</label></td>
-        <td><input id="last_name" name="last_name" required></td>
+        <td><input id="last_name" name="register[last_name]" required></td>
     </tr>
     <tr>
         <td><label for="password">Password:</label></td>
-        <td><input id="password" name="password" type="password" required></td>
+        <td><input id="password" name="register[password][0]" type="password" required></td>
     </tr>
     <tr>
         <td><label for="confirm_password">Confirm Password:</label></td>
-        <td><input id="confirm_password" name="confirm_password" type="password" required></td>
+        <td><input id="confirm_password" name="register[password][1]" type="password" required></td>
     </tr>
     <tr>
         <td><label for="email">Email:</label></td>
-        <td><input id="email" name="email" type="email" required></td>
+        <td><input id="email" name="register[email]" type="email" required></td>
     </tr>
     <tr>
         <td><label for="phone_number">Phone Number:</label></td>
-        <td><input id="phone_number" name="phone_number" type="tel" required></td>
+        <td><input id="phone_number" name="register[phone_number]" type="tel" required></td>
     </tr>
     <tr>
         <td>Address:</td>
     </tr>
     <tr>
         <td><label for="street">Street Address:</label></td>
-        <td><input id="street" name="street" required></td>
+        <td><input id="street" name="register[street]" required></td>
     </tr>
     <tr>
         <td><label for="apartment_number">APT #:</label></td>
-        <td><input id="apartment_number" name="apartment_number" type="number"></td>
+        <td><input id="apartment_number" name="register[apartment_number]" type="number"></td>
     </tr>
     <tr>
         <td><label for="city">City:</label></td>
-        <td><input id="city" name="city" required></td>
+        <td><input id="city" name="register[city]" required></td>
     </tr>
     <tr>
-        <td><label for="state/province">State / Province:</label></td>
-        <td><input id="state/province" name="state/province" required></td>
+        <td><label for="state">State / Province:</label></td>
+        <td><input id="state" name="register[state]" required></td>
     </tr>
     <tr>
         <td><label for="postal_code">Postal Code:</label></td>
-        <td><input id="postal_code" type="number" maxlength="5" minlength="5" required></td>
+        <td><input id="postal_code" name="register[zip]" type="number" maxlength="5" minlength="5" required></td>
     </tr>
 </table>
 <script>
