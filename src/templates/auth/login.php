be0c9f55c65a0180fa364f6ee01ff251936a933f
Emmet Stanevich <emmet@stanevich.com>
2021-04-03 16:06:20 -0400

    Added names to email and password fields in login page

--- src/templates/auth/login.php
+++ src/templates/auth/login.php
@@ -5,11 +5,11 @@
 <table>
     <tr>
         <td><label for="email">Email:</label></td>
-        <td><input id="email" type="email" required></td>
+        <td><input id="email" name="login[email]" type="email" required></td>
     </tr>
     <tr>
         <td><label for="password">Password:</label></td>
-        <td><input id="password" type="password" required></td>
+        <td><input id="password" name="login[password]" type="password" required></td>
     </tr>
 </table>
 
