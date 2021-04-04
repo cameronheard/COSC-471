3a9bae17b9cce2b3f35b0e854a4e4350f109912a
Emmet Stanevich <emmet@stanevich.com>
2021-04-03 22:35:15 -0400

    Implemented login page
    This page will allow an existing user to sign into their account.

--- src/login.php
+++ src/login.php
@@ -1,12 +1,52 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (!empty($_POST["email"]) && !empty($_POST["password"])) {
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-    } else {
-        array_push($_SESSION["errors"], "Missing email/password!");
+function login() {
+    global $errors;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $login = $_POST["login"];
+            if (empty($login["email"])) array_push($errors, "Missing email!");
+            else $email = $login["email"];
+            if (empty($login["password"])) array_push($errors, "Missing password!");
+            else $password = $login["password"];
+
+            switch ($login["type"]) {
+                case "customer":
+                    $verify_user_exists_query = "SELECT COUNT(*) FROM Customer WHERE Email = :email;";
+                    $retrieve_user_query = "SELECT ID, Password FROM Customer WHERE Email = :email LIMIT 1;";
+                    break;
+                case "seller":
+                    $verify_user_exists_query = "SELECT COUNT(*) FROM Seller WHERE Email = :email;";
+                    $retrieve_user_query = "SELECT ID, Password FROM Seller WHERE Email = :email LIMIT 1;";
+                    break;
+                default:
+                    array_push($errors, "No account type selected!");
+                    header("Location: " . $_SERVER["REQUEST_URI"]);
+                    die;
+            }
+
+            if (!empty($errors)) {
+                // If any errors are present, terminate the script immediately, allowing the errors to be displayed and cleared.
+                header("Location: " . $_SERVER["REQUEST_URI"]);
+                die;
+            }
+
+            $db = connect_to_database();
+
+            $query = $db->prepare($retrieve_user_query);
+            $query->bindParam(":email", $email);
+            $query->execute();
+            $user = $query->fetch(PDO::FETCH_ASSOC);
+
+            if (!empty($user)) {
+                if (password_verify($password, $user["Password"])) {
+                    $_SESSION["user"] = ["id" => $user["ID"], "account_type" => $login["type"]];
+                }
+            } else array_push($errors, "No user found!");
+
+        header("Location: {$_SERVER['PHP_SELF']}");
+        exit;
     }
-    header("Location: {$_SERVER['PHP_SELF']}");
+
+    require __DIR__ . "/templates/auth/login.php";
 }
 
-require __DIR__ . "/templates/auth/login.php";
+login();
