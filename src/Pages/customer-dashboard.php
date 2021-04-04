891b53348e1b22621e30116b8ddc685f507913dc
Abdullah Najid <anajid@emich.edu>
2021-04-04 18:23:51 -0400

    Added customer-dashboard page, customer-find-products page, and a barebones customer-my-info page

---
+++ src/Pages/customer-dashboard.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <?php
+        // Get Customer's first name code here.
+        echo "<title>CUSTOMER_NAME's Dashboard</title>";
+        ?>
+		
+	</head>
+	<body class="body">
+        <?php
+        // Get Customer's first name code here.
+        echo "<h2 class='header'>CUSTOMER_NAME's Dashboard</h2>";
+        ?>
+        
+        <button onclick="document.location='customer-find-products.php'">
+        Find Products
+        </button><br>
+        
+        <button onclick="document.location='MyOrdersPageLayout.htm'">
+        My Orders
+        </button><br>
+        
+		<button onclick="document.location='customer-my-info.php'">
+        My Info
+        </button><br>
+	</body>
+
+	
+</html>
