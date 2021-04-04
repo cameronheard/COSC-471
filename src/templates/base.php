4391cef935fb22967f20cf26ee4c0485ebca2f36
Emmet Stanevich <emmet@stanevich.com>
2021-04-04 19:00:40 -0400

    Moved errors into separate JSON code block
    This will make it a bit easier to move the logic into a separate file.

--- src/templates/base.php
+++ src/templates/base.php
@@ -7,9 +7,11 @@
 <body>
 <?php include __DIR__ . "/navbar.php" ?>
 <?php if (!empty($_SESSION["errors"])): ?>
+    <script id="errors"
+            type="application/json"><?= json_encode($_SESSION["errors"])  // Encodes the list of errors into a JSON array.  ?></script>
     <script>
         {
-            let errors = <?= json_encode($_SESSION["errors"])  // Encodes the list of errors into a JSON array. ?>;  // Reads the provided JSON array from the server.
+            let errors = JSON.parse(document.getElementById("errors").text);  // Reads the provided JSON array from the server.
             <?php unset($_SESSION["errors"])  // After retrieving the errors, remove them from the session ?>
             alert(errors.join("\n"));  // Alerts the user to the error(s).
         }
