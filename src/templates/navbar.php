f049b7379db6cbe9d4086833da34a731b50a7577
Emmet Stanevich <emmet@stanevich.com>
2021-04-06 12:56:21 -0400

    Added links to "my-info" and "products" pages
    I may have to rearrange them later, maybe put the "my-info" link behind a conditional check.

--- src/templates/navbar.php
+++ src/templates/navbar.php
@@ -1,5 +1,7 @@
 <nav class="topnav">
     <span id="title"><?= htmlspecialchars($title ?? "Empty Page") ?></span>
+    <a href="/index.php/my-info">My Info</a>
+    <a href="/index.php/products">Products</a>
     <div id="auth_links">
         <?php if (!isset($_SESSION["user"])): ?>
             <a class="auth_link" href="/index.php/register">Register</a>
