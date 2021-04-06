92d6f7fc8584360021599092156609634333a92a
jpoeana1 <jpoeana1@emich.edu>
2021-04-06 12:23:05 -0400

    Use this script if all else fails.

--- src/includes/db_init.php
+++ src/includes/db_init.php
@@ -9,7 +9,7 @@ if(!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = 'CREATE DATABASE `eMarket`;';
+$sql = 'CREATE DATABASE IF NOT EXISTS eMarket;';
 
 if(mysqli_query($conn, $sql)) {
   echo "Database has been created";
@@ -238,6 +238,15 @@ alter table Product
     add constraint Product_Seller_StoreID_SellerID_fk
         foreign key (StoreID, SellerID) references Seller (StoreID, SellerID)
             on update cascade on delete cascade;
+
+            
+INSERT INTO `Courier`(`CourierID`, `CourierName`, `Street`, `Apt`, `City`, `StateProvince`, `PostalCode`, `Country`, `CourierPhoneNo`) VALUES (1, "USPS", "7762 Somerset St", 348, "Randolf", "MA", 2368, "USA", "2576017248");
+INSERT INTO `Customer`(`CustomerID`, `Name`, `PhoneNo`, `Street`, `City`, `Zip`, `State`, `Email`) VALUES (1, "Stu Oarslogs", "7382849144", "395 West Wood Rd", "Tampa", 33470, "FL", "StuLogIn@gmail.com");
+INSERT INTO `Store`(`StoreID`, `StoreName`, `Street`, `Apt`, `City`, `StateProvince`, `PostalCode`, `Country`, `StoreEmail`, `StorePhoneNo`, `StoreDescription`) VALUES (1, "Mains-tree-m Resources", "026 Willo Way", 567, "Oakspeak", "CO", 95395, "USA", "WoodMinistree@hotmail.com", "4371564840", "Sells lots of sticks");
+INSERT INTO `Seller`(`SellerID`, `SellerName`, `Street`, `Apt`, `City`, `StateProvince`, `PostalCode`, `Country`, `SellerEmail`, `SellerPhoneNo`, `StoreID`) VALUES (1, "Benjamin K", "971 Sawzal Lane", 302, "Waxahatchet", "FL", 33470, "USA", "TimberMerch@gmail.com", "7382849144", 1);
+INSERT INTO `Product`(`ProductID`, `ProductName`, `Price`, `Stock`, `SellerID`, `StoreID`) VALUES (1, "Stick", 10, 55, 1, 1);
+INSERT INTO `Orders`(`OrderID`, `Cost`, `OrderDate`, `CustomerID`, `ProductID`, `CourierID`) VALUES (1, 20, "2021-4-1", 1, 1, 1);
+INSERT INTO `Payment`(`PaymentID`, `PaymentAmount`, `PaymentType`, `CustomerID`) VALUES (1, 100, "Debit", 1);
 ';
 
 if(mysqli_multi_query($conn, $sql)) {
