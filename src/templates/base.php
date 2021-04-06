f5c06cff1b7fa3ea43841a45be56d9c30ccfc261
Emmet Stanevich <emmet@stanevich.com>
2021-04-06 12:54:06 -0400

    Moved script import to head

--- src/templates/base.php
+++ src/templates/base.php
@@ -3,15 +3,15 @@
 <head>
     <title><?= $title ?? "Empty Page" ?></title>
     <link rel="stylesheet" href="/static/base.css" type="text/css">
+    <?php if (!empty($_SESSION["errors"])): ?>
+        <script id="errors"
+                type="application/json"><?= json_encode($_SESSION["errors"])  // Encodes the list of errors into a JSON array.  ?></script>
+    <?php unset($_SESSION["errors"])  // After retrieving the errors, remove them from the session ?>
+        <script defer src="../static/scripts/display_errors.js"></script>
+    <?php endif; ?>
 </head>
 <body>
 <?php include __DIR__ . "/navbar.php" ?>
-<?php if (!empty($_SESSION["errors"])): ?>
-    <script id="errors"
-            type="application/json"><?= json_encode($_SESSION["errors"])  // Encodes the list of errors into a JSON array.  ?></script>
-    <?php unset($_SESSION["errors"])  // After retrieving the errors, remove them from the session ?>
-    <script defer src="scripts/display_errors.js"></script>
-<?php endif; ?>
 <div id="content">
     <?= $content ?? "" ?>
 </div>
