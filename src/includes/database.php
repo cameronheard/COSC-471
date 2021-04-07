2d8577ffb89aeb949344982c3d580c1724a7999d
Emmet Stanevich <emmet@stanevich.com>
2021-04-07 15:17:16 -0400

    Added null check for password in database.php
    I could have sworn I had already committed this change, but I guess I had forgotten.

--- src/includes/database.php
+++ src/includes/database.php
@@ -6,7 +6,7 @@
 function connect_to_database(): PDO
 {
     $username = isset($_ENV["MYSQL_USER_FILE"]) ? file_get_contents($_ENV["MYSQL_USER_FILE"]) : $_ENV["MYSQL_USER"] ?? "root";
-    $password = isset($_ENV["MYSQL_PASSWORD_FILE"]) ? file_get_contents($_ENV["MYSQL_PASSWORD_FILE"]) : $_ENV["MYSQL_PASSWORD"];
+    $password = isset($_ENV["MYSQL_PASSWORD_FILE"]) ? file_get_contents($_ENV["MYSQL_PASSWORD_FILE"]) : $_ENV["MYSQL_PASSWORD"] ?? null;
     $dbHost = $_ENV['DB_HOST'] ?? 'localhost';
     $dbName = $_ENV['DB_NAME'] ?? 'eMarket';
     $dbPort = $_ENV['DB_PORT'] ?? 3306;
