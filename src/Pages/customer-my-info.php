5c997ec912eb0f027682c8fb34df823d21ed6a25
Valerie Schafer <valerie.schafer@emich.edu>
2021-04-08 18:30:34 -0400

    Updated css for customer info page

--- src/Pages/customer-my-info.php
+++ src/Pages/customer-my-info.php
@@ -7,12 +7,29 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="eMarketDefault.css">
+    <style>
+			.body {
+				background-color: rgba(253, 253, 253, 0.966);
+				border-style: solid;
+				width: 400px;
+				height: 550px;
+				text-align: center;
+				position: fixed;
+				top: 50%;
+				left: 50%;
+				margin-top: -300px;
+				margin-left: -200px;
+			}
+			.header {
+				background-color: rgba(253, 253, 253, 0.966);
+			}
+
+		</style>
     <title><?php getCustomerName($customerID);?>'s Info</title>
 </head>
-<body>
+<body class="body">
 
-<h1><?php getCustomerName($customerID);?>'s Info</h1>
+<h1 class="header"><?php getCustomerName($customerID);?>'s Info</h1>
 
 <?php printCustomerInformation($customerID);?>
 
