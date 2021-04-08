1bf16b84d7a2fe1ec3749a8baa654d92bf4337b9
Emmet Stanevich <emmet@stanevich.com>
2021-04-07 20:47:44 -0400

    Added navbar to customer dashboard page

--- src/Pages/customer-dashboard.php
+++ src/Pages/customer-dashboard.php
@@ -9,10 +9,12 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="eMarketDefault.css">
+    <link rel="stylesheet" href="/COSC-471/src/static/css/base.css">
     <title><?php getCustomerName($customerID);?>'s Dashboard</title>
 </head>
 
 <body class="body">
+    <?php include __DIR__ . "/../templates/navbar.php"; ?>
     
     <h1 class='header'><?php getCustomerName($customerID);?>'s Dashboard</h1>
     <button onclick="document.location='customer-find-products.php'">
